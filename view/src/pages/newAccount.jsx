bb74e6b1eea0c776045e8edda038edf1f66f9e4b
AbayoHJovin <abayohirwajovin@gmail.com>
2024-09-26 12:07:53 +0200

    Changed the cards

--- view/src/pages/newAccount.jsx
+++ view/src/pages/newAccount.jsx
@@ -18,7 +18,7 @@ const NewAccount = () => {
   const { currentUser } = useContext(CurrentUserContext);
   const navigate = useNavigate();
   const [isSignedIn, setIsSignedIn] = useState(false);
-  const [isLoggingOut,setIsLoggingOut]=useState(false)
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -90,30 +90,33 @@ const NewAccount = () => {
   ];
 
   return (
-    <div className={`dark:bg-black bg-white`}>
+    <div className={`bg-gray-200 dark:bg-black`}>
       {isSignedIn ? (
-        <Sidebar labels={labels} handleConfirmLogout={handleConfirmLogout} isLoggingOut={isLoggingOut}/>
+        <Sidebar
+          labels={labels}
+          handleConfirmLogout={handleConfirmLogout}
+          isLoggingOut={isLoggingOut}
+        />
       ) : (
-        <div className=" text-black dark:text-white bg-white dark:bg-black">
-          <div className="my-10">
-            <div className="flex flex-col justify-center items-start ssm:items-center text-start ssm:text-center p-5 font-bold text-xl">
-              <h1 className="ssm:text-center">
-                Dear user, You are not signed in. Better Create an account
+        <div className="flex justify-center items-center min-h-screen text-black dark:text-white p-5">
+          <div className="mb-10 p-5 sm:p-10 rounded-lg bg-white dark:bg-black w-full max-w-md">
+            <div className="flex flex-col justify-center items-start ssm:items-center text-start ssm:text-center">
+              <h1 className="ssm:text-center my-5 font-bold text-2xl sm:text-xl">
+                Not signed in!
               </h1>
-              <h1 className="self-start ssm:self-center mt-4 ssm:mt-2">Or</h1>
-              <h1 className="ssm:text-center">
-                Login into your account if you want.
+              <h1 className="ssm:text-center text-base sm:text-lg">
+                Log in or create an account to improve your shopping experience.
               </h1>
             </div>
 
-            <div className="flex p-5 flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-5 my-10">
+            <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-5 mt-5">
               <a href="/signup">
-                <button className="bg-green-900 text-white py-2 px-4 sm:py-3 sm:px-5 rounded-md text-sm sm:text-base">
+                <button className="bg-[#6ed629] text-white py-2 px-4 sm:py-3 sm:px-5 rounded-md text-sm sm:text-base">
                   Sign Up
                 </button>
               </a>
               <a href="/login">
-                <button className="bg-green-900 text-white py-2 px-4 sm:py-3 sm:px-5 rounded-md text-sm sm:text-base">
+                <button className="bg-[#6ed629] text-white py-2 px-4 sm:py-3 sm:px-5 rounded-md text-sm sm:text-base">
                   Login
                 </button>
               </a>
