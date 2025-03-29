9ff26dec3904890797136f047176809a249dc5c7
AbayoHJovin <abayohirwajovin@gmail.com>
2025-03-29 16:24:17 +0200

    ff

--- controller/socket.js
+++ controller/socket.js
@@ -1,8 +1,7 @@
 const port = 3000; // Use a non-privileged port
 const io = require("socket.io")(port, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: ["http://localhost:5173","http://localhost:5174", "https://homedel-jov.vercel.app/"] },
 });
-
 // Log when a client connects
 io.on("connection", (socket) => {
   console.log(`Client connected: ${socket.id}`);
